91e81d7e894dc3b722a0f105a505a67106aa3001
manjosh03 <manjosh01@gmail.com>
2024-05-30 12:16:29 -0700

    Added initial files

---
+++ src/main/java/com/assgn1/assgn1/Assgn1Application.java
@@ -0,0 +1,14 @@
+package com.assgn1.assgn1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assgn1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assgn1Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/assgn1/assgn1/Assgn1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assgn1.assgn1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assgn1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
